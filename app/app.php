6fc212d4d9a796ddc74a35e08c58aca852bec189
John <student@epicodus.com>
2015-06-22 16:49:47 -0700

    save on Monday PM

---
+++ app/app.php
@@ -0,0 +1,34 @@
+<?php
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Task.php";
+
+    $app = new Silex\Application();
+
+    $database = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $DB = new PDO($database, $username);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('tasks.php', array('tasks' => Task::getALL()));
+    });
+
+    $app->post("/tasks", function() use ($app) {
+        $task = new Task($_POST['description']);
+        $task->save();
+
+        return $app['twig']->render('create_task.php', array('newtask' => $task));
+
+    });
+
+    $app->post("/delete_tasks", function() use ($app) {
+        Task::deleteAll();
+        return $app['twig']->render('delete_tasks.php');
+    });
+
+    return $app;
+?>
---
+++ src/Task.php
@@ -0,0 +1,59 @@
+<?php
+class Task
+{
+    private $description;
+    private $id;
+
+    function __construct($description, $id = null)
+    {
+        $this->description = $description;
+        $this->id = $id;
+    }
+
+    function setDescription($new_description)
+    {
+        $this->description = (string) $new_description;
+    }
+
+    function getDescription()
+    {
+        return $this->description;
+    }
+
+    function getId()
+    {
+      return $this->id;
+    }
+
+    function setId($new_id)
+    {
+      $this->id = (int) $new_id;
+    }
+
+    function save()
+    {
+      $GLOBALS['DB']->query("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
+      $statement = $GLOBALS['DB']->query("SELECT LAST_INSERT_ID();");
+      $result = $statement->fetch(PDO::FETCH_ASSOC);
+      $this->setId(mysql_insert_id());
+    }
+
+    static function getAll()
+    {
+      $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+      $tasks = array();
+      foreach($returned_tasks as $task) {
+        $description = $task['description'];
+        $id = $task['id'];
+        $new_task = new Task($description, $id);
+        array_push($tasks, $new_task);
+      }
+      return $tasks;
+    }
+
+    static function deleteAll()
+    {
+      $GLOBALS['DB']->exec("DELETE FROM tasks;");
+    }
+}
+?>
