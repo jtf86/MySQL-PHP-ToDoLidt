c45d8752a38d797138f12eead3ffa392e171a80d
John <john@epicodus.com>
2015-08-18 11:04:09 -0700

    Add relationship functions

--- src/Task.php
+++ src/Task.php
@@ -1,66 +1,73 @@
 <?php
-class Task
-{
-  private $description;
-  private $id;
+    class Task
+    {
+        private $description;
+        private $category_id;
+        private $id;
 
-  function __construct($description, $id = null)
-  {
-    $this->description = $description;
-    $this->id = $id;
-  }
+        function __construct($description, $id = null, $category_id)
+        {
+            $this->description = $description;
+            $this->id = $id;
+            $this->category_id = $category_id;
+        }
 
-  function setDescription($new_description)
-  {
-    $this->description = (string) $new_description;
-  }
+        function setDescription($new_description)
+        {
+            $this->description = (string) $new_description;
+        }
 
-  function getDescription()
-  {
-    return $this->description;
-  }
+        function getDescription()
+        {
+            return $this->description;
+        }
 
-  function getId()
-  {
-    return $this->id;
-  }
+        function getId()
+        {
+            return $this->id;
+        }
 
-  function save()
-  {
-    $GLOBALS['DB']->exec("INSERT INTO tasks (description) VALUES ('{$this->getDescription()}');");
-    $this->id = $GLOBALS['DB']->lastInsertId();
-  }
+        function getCategoryId()
+        {
+            return $this->category_id;
+        }
 
-  static function getAll()
-  {
-    $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
-    $tasks = array();
-    foreach($returned_tasks as $task) {
-      $description = $task['description'];
-      $id = $task['id'];
-      $new_task = new Task($description, $id);
-      array_push($tasks, $new_task);
+        function save()
+        {
+            $statement = $GLOBALS['DB']->exec("INSERT INTO tasks (description, category_id) VALUES ('{$this->getDescription()}', {$this->getCategoryId()})");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_tasks = $GLOBALS['DB']->query("SELECT * FROM tasks;");
+            $tasks = array();
+            foreach($returned_tasks as $task) {
+                $description = $task['description'];
+                $id = $task['id'];
+                $category_id = $task['category_id'];
+                $new_task = new Task($description, $id, $category_id);
+                array_push($tasks, $new_task);
+            }
+            return $tasks;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM tasks;");
+        }
+
+        static function find($search_id)
+        {
+            $found_task = null;
+            $tasks = Task::getAll();
+            foreach($tasks as $task) {
+                $task_id = $task->getId();
+                if ($task_id == $search_id) {
+                  $found_task = $task;
+                }
+            }
+            return $found_task;
+        }
     }
-    return $tasks;
-  }
-
-  static function deleteAll()
-  {
-    $GLOBALS['DB']->exec("DELETE FROM tasks;");
-  }
-
-  static function find($search_id)
-  {
-      $found_task = null;
-      $tasks = Task::getAll();
-      foreach($tasks as $task) {
-          $task_id = $task->getId();
-          if ($task_id == $search_id) {
-              $found_task = $task;
-          }
-      }
-      return $found_task;
-  }
-
-}
 ?>
